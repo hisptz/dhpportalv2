5bbec4dda5b785dc13d6eca0ebec7cefa525d42c
Leonard Mpande <leo.august27@gmail.com>
2016-02-20 08:57:45 +0300

    resolve watch

--- services/mapService.js
+++ services/mapService.js
@@ -124,14 +124,14 @@ angular.module("dhpportal")
             var names = nalaytics_data.metaData.names;
 
             if(rows.length<=0){
-                return color;
+//                return color;
             }else if(rows.length>0){
                 if(map.isCompleted(rows,value,completenes)){
                     color = 'green';
                 }
 
             }
-
+            return color;
 
         }
 
