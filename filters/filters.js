9b652a7875d8fae860bea53ccba2b4438567c402
leompande <leo.august27@gmail.com>
2016-03-04 10:07:05 +0300

    troubleshooting

--- filters/filters.js
+++ filters/filters.js
@@ -12,6 +12,32 @@
             }
 
         }
+    )
+    .filter('addElements', function (){
+
+            var sum = 0;
+            return function(input,param1)
+            {
+                var args = Array.prototype.slice.call(arguments);
+                angular.forEach(args,function(value,index){
+
+                    if(isNaN(value)){
+
+                    }else{
+                        sum = sum+Number(value);
+                        if(index==0){
+                           sum = sum-Number(value);
+                        }
+                    }
+                });
+
+                if(sum==0){
+                    return 0;
+                }
+                return sum;
+            }
+
+        }
     );
 
 
