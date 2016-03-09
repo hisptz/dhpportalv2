4146043d28ee35780bd2c4f819f315428e066409
leompande <leo.august27@gmail.com>
2016-03-09 16:16:22 +0300

    resources

--- services/dataService.js
+++ services/dataService.js
@@ -9,7 +9,7 @@
         var dataService = this;
         dataService.baseDHIS = profileService.baseDHIS;
         dataService.getPopulationData = function(orgunit,period){
-            return $http.get(dataService.baseDHIS+"api/analytics.json?dimension=Cow9nZikDgD:FfN1mqXvpR7;HKU7NijIEIH;LBipXEMD6mq;aZcKJ9XxvaF;h8JRv8POdfy;p1b4SYcdjJw&dimension=dx:ykShMtNgDB1&dimension=hENn80Fmmlf:mtUMlCLFTTz;syxWmui9UMq&filter=ou:"+orgunit+"&filter=pe:"+period).then(handleSuccess, handleError('Error loading population data'));
+            return $http.get("https://hmisportal.moh.go.tz/dhis/"+"api/analytics.json?dimension=Cow9nZikDgD:FfN1mqXvpR7;HKU7NijIEIH;LBipXEMD6mq;aZcKJ9XxvaF;h8JRv8POdfy;p1b4SYcdjJw&dimension=dx:ykShMtNgDB1&dimension=hENn80Fmmlf:mtUMlCLFTTz;syxWmui9UMq&filter=ou:"+orgunit+"&filter=pe:"+period).then(handleSuccess, handleError('Error loading population data'));
         }
 
         return dataService;
