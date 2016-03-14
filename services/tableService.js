7992bab4a7f01c39576cb49ed1505208609993e0
leompande <leo.august27@gmail.com>
2016-03-14 15:16:17 +0300

    pushong only existing fiules

--- services/tableService.js
+++ services/tableService.js
@@ -16,13 +16,35 @@
 
             angular.forEach(data,function(value){
                 if(features[value.id].color=="green"){
-                    tableobject.push({name:features[value.id].name,url:'uploads/'+features[value.id].name+'_'+selectedYear+'.pdf'});
+
+                    if(doesFileExist('uploads/'+features[value.id].name+'_'+selectedYear+'.pdf')){
+                        tableobject.push({name:features[value.id].name,url:'uploads/'+features[value.id].name+'_'+selectedYear+'.pdf'});
+                    }else{
+
+                    }
+
                 }
             })
 
             return tableobject;
         }
+
+
+        function doesFileExist(urlToFile)
+        {
+            var xhr = new XMLHttpRequest();
+            xhr.open('HEAD', urlToFile, false);
+            xhr.send();
+
+            if (xhr.status == "404") {
+                return false;
+            } else {
+                console.log('file doesnot exist');
+                return true;
+            }
+        }
         return tableService;
     }
+
 
 })();
