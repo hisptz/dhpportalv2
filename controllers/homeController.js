f53532aea77561aeddd987d6bb2dda9a56292e12
leompande <leo.august27@gmail.com>
2016-08-29 19:45:31 +0300

    working on the suggested changes

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -5,5 +5,12 @@ angular.module("dhpportal")
 .$inject   = ['$scope','$rootScope','$cookies','$filter','$http','$timeout','$interval','$location','DTOptionsBuilder', 'DTColumnDefBuilder','dataService','profileService','utilityService','portalService','chartService','olData','olHelpers','mapService']
 .controller("homeController",function ($scope,$rootScope,$cookies,$filter,$http,$timeout,$interval,$location,DTOptionsBuilder, DTColumnDefBuilder,dataService,profileService,utilityService,portalService,chartService,olData,olHelpers,mapService) {
 
+    $scope.printProfile = function(divName) {
+        var printContents = document.getElementById(divName).innerHTML;
+        var originalContents = document.body.innerHTML;
+        document.body.innerHTML = printContents;
+        window.print();
+        document.body.innerHTML = originalContents;
+    }
 
 });
