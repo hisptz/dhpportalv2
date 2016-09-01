df8c0725428fea53b0a434ecbea5856393d09afc
leompande <leo.august27@gmail.com>
2016-09-01 17:25:17 +0300

    revamp load bug

--- controllers/downloadableController.js
+++ controllers/downloadableController.js
@@ -5,6 +5,7 @@
 angular.module("dhpportal")
     .controller("downloadableController",function ($rootScope,$scope,$q,$http,$timeout,portalService,dataService) {
         $rootScope.updateDataContainers = function(){
+            console.log(dataService);
             dataService.loadAllFiles().then(function(files){
                 console.log(files);
             },function(failure){
