8486babbd63fc2da2139fc8342261dea66ac9b52
leompande <leo.august27@gmail.com>
2016-09-03 15:35:42 +0300

    adding document viewer

--- controllers/downloadableController.js
+++ controllers/downloadableController.js
@@ -1,38 +1,46 @@
 /**
- * Created by kelvin on 10/20/15.
+ * Created by Leonard C. Mpande on 31/08/2016.
  */
 
 angular.module("dhpportal")
     .controller("downloadableController",['$rootScope','$scope','$routeParams','$q','$http','$timeout','portalService','dataService',function ($rootScope,$scope,$routeParams,$q,$http,$timeout,portalService,dataService) {
-        $rootScope.updateDataContainers = function(){
 
-            $scope.treeOptions = {
-                nodeChildren: "children",
-                dirSelectable: true,
-                injectClasses: {
-                    ul: "a1",
-                    li: "a2",
-                    liSelected: "a7",
-                    iExpanded: "a3",
-                    iCollapsed: "a4",
-                    iLeaf: "a5",
-                    label: "a6",
-                    labelSelected: "a8"
-                }
-            }
-            console.log($routeParams);
-            if ( $routeParams.parentUid )
+      if ( $routeParams.parentUid )
+      {
+        $rootScope.showBackButton = true;
+      }else{
+        $rootScope.showBackButton = false;
+      }
+
+      $rootScope.updateDataContainers = function(){
+            if ( $routeParams.parentUid != $rootScope.selectedOrgUnit )
             {
-              $rootScope.showBackButton = true;
-            }else{
-              $rootScope.showBackButton = false;
+                window.location.href = "#/home/downloadable/"+$rootScope.selectedOrgUnit+"/"+$rootScope.selectedYear;
+            }else {
+
+              $scope.treeOptions = {
+                  nodeChildren: "children",
+                  dirSelectable: true,
+                  injectClasses: {
+                      ul: "a1",
+                      li: "a2",
+                      liSelected: "a7",
+                      iExpanded: "a3",
+                      iCollapsed: "a4",
+                      iLeaf: "a5",
+                      label: "a6",
+                      labelSelected: "a8"
+                  }
+              }
+
+              dataService.loadAllFiles().then(function(files){
+                $scope.dataForTheTree = dataService.formatDataForTree(files);
+              },function(failure){
+
+              })
+
             }
 
-            dataService.loadAllFiles().then(function(files){
-              $scope.dataForTheTree = dataService.formatDataForTree(files);
-            },function(failure){
-
-            })
         }
 
     }]);
