edcf2e47c4a2f85d6a54fe966fa0334909f5c628
leompande <leo.august27@gmail.com>
2016-09-09 16:13:13 +0300

    refining documents viewer

--- services/dataService.js
+++ services/dataService.js
@@ -178,7 +178,7 @@
 
         dataService.formatDataForTree  =  function(files,organisationUnitTree) {
 
-                    var treeData = [{name:organisationUnitTree[0].name,children:null}];
+                    var treeData = [{children:[{name:organisationUnitTree[0].name,children:null}}]];
                        var regions = [];
                        var regionsArray = [];
 
