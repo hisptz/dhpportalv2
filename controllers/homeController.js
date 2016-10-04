d4ee864010791bcc3b83a3767ee254df9eaf2092
leompande <leo.august27@gmail.com>
2016-10-04 11:04:40 +0300

    adding map for files check

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -19,7 +19,7 @@
 
 
         $rootScope.upDateProfileView = function(selectedItem,selectedYear){
-          console.log(selectedItem,selectedYear);
+          console.log(organisationUnitTree,selectedItem,selectedYear);
         }
 
 
